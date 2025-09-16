ce3eac0fcdc192e0d2ee834e2959f4274aa7bf21
Francisco Pieper <fapieper@miuandes.cl>
2025-09-16 12:24:30 -0300

    feat: Settings to use PostgreSQL with .env variables

--- salesorder_manager/salesorder_manager/settings.py
+++ salesorder_manager/salesorder_manager/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DBNAME = os.environ.get('DBNAME')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,9 +78,12 @@ WSGI_APPLICATION = 'salesorder_manager.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "port": 5432,
+            "dbname": DBNAME
+        },
     }
 }
 
