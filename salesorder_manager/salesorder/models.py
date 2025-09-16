9b5e3555655c05b8c3c4bff0ca6a83c4dd6de7c3
Francisco Pieper <fapieper@miuandes.cl>
2025-09-16 14:26:14 -0300

    feat: Added initial models

--- salesorder_manager/salesorder/models.py
+++ salesorder_manager/salesorder/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Product(models.Model):
+    brand = models.CharField(max_length=50,default="Regular Brand")
+    product_type = models.CharField(max_length=50)
+    name = f"{brand} {product_type}"
+    base_price = models.IntegerField()
+
+    @property
+    def full_name(self):
+        return f"{self.brand} {self.product_type}"
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class SalesOrder(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    created_at = models.DateField(default=datetime.now)
+    product = models.ManyToManyField(Product, through="OrderItem")
+    status = models.TextChoices("In process", "Completed", "Canceled",)
+    
+class OrderItem(models.Model):
+    sales_order = models.ForeignKey(SalesOrder, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    item_discount = models.DecimalField()
+    quantity = models.PositiveIntegerField(default=0)
+
+    @property
+    def product_price(self):
+        return self.product.base_price
