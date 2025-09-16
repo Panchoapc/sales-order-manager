b2b55af350b53d686d107cabc14e2b2e49a46128
Francisco Pieper <fapieper@miuandes.cl>
2025-09-16 17:36:46 -0300

    feat: added admin actions with current models

--- salesorder_manager/salesorder/admin.py
+++ salesorder_manager/salesorder/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SalesOrder, Client, Product, OrderItem
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+class SalesOrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemAdmin]
+    
+
+
+admin.site.register(SalesOrder, SalesOrderAdmin)
+admin.site.register(Client)
+admin.site.register(Product)
--- salesorder_manager/salesorder/models.py
+++ salesorder_manager/salesorder/models.py
@@ -8,28 +8,40 @@ class Product(models.Model):
     base_price = models.IntegerField()
 
     @property
-    def full_name(self):
-        return f"{self.brand} {self.product_type}"
+    def full_name(self) -> str:
+        return f"{self.brand} - {self.product_type}"
+    
+    def __str__(self) -> str:
+        return self.full_name
 
 class Client(models.Model):
     name = models.CharField(max_length=100, null=False)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
     
-
 class SalesOrder(models.Model):
+    IN_PROCESS = "IN PROCESS"
+    COMPLETED = "COMPLETED"
+    CANCELED = "CANCELED"
+    ORDER_STATUS_CHOICES = {
+        IN_PROCESS: "In process",
+        COMPLETED: "Completed",
+        CANCELED: "Canceled",
+    }
     client = models.ForeignKey(Client, on_delete=models.CASCADE, null=False)
-    created_at = models.DateField(default=datetime.now)
-    product = models.ManyToManyField(Product, through="OrderItem")
-    status = models.TextChoices("In process", "Completed", "Canceled",)
+    created_at = models.DateTimeField(default=datetime.now)
+    products = models.ManyToManyField(Product, through="OrderItem")
+    status = models.CharField(choices=ORDER_STATUS_CHOICES, default=IN_PROCESS)
+
+    def __str__(self):
+        return f"{self.created_at.day}/{self.created_at.month} - {self.client} - {self.status}"
     
+
 class OrderItem(models.Model):
     sales_order = models.ForeignKey(SalesOrder, on_delete=models.CASCADE, null=False)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
-    quantity = models.PositiveIntegerField(default=0, null=False)
+    product_price = models.IntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=1, null=False)
     item_discount = models.IntegerField(default=0)
-
-    @property
-    def product_price(self):
-        return self.product.base_price
+    
