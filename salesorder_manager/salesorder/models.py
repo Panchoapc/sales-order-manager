d2bc9dabd0b711ad8c69f8264203586d83579364
Francisco Pieper <fapieper@miuandes.cl>
2025-09-16 15:26:52 -0300

    feat: Models migrations

--- salesorder_manager/salesorder/models.py
+++ salesorder_manager/salesorder/models.py
@@ -3,9 +3,8 @@ from datetime import datetime
 
 # Create your models here.
 class Product(models.Model):
-    brand = models.CharField(max_length=50,default="Regular Brand")
-    product_type = models.CharField(max_length=50)
-    name = f"{brand} {product_type}"
+    brand = models.CharField(max_length=50,default="Regular Brand", null=False)
+    product_type = models.CharField(max_length=50, null=False)
     base_price = models.IntegerField()
 
     @property
@@ -13,23 +12,23 @@ class Product(models.Model):
         return f"{self.brand} {self.product_type}"
 
 class Client(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=False)
 
     def __str__(self):
         return self.name
     
 
 class SalesOrder(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, null=False)
     created_at = models.DateField(default=datetime.now)
     product = models.ManyToManyField(Product, through="OrderItem")
     status = models.TextChoices("In process", "Completed", "Canceled",)
     
 class OrderItem(models.Model):
-    sales_order = models.ForeignKey(SalesOrder, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    item_discount = models.DecimalField()
-    quantity = models.PositiveIntegerField(default=0)
+    sales_order = models.ForeignKey(SalesOrder, on_delete=models.CASCADE, null=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
+    quantity = models.PositiveIntegerField(default=0, null=False)
+    item_discount = models.IntegerField(default=0)
 
     @property
     def product_price(self):
